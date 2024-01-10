8331ca08346425648ae5ae434cf58f280a10036f
erickseis <erickseis@gmail.com>
2024-01-10 14:05:19 -0300

    carga

--- main.js
+++ main.js
@@ -6,7 +6,14 @@ document.getElementById('scrollLeft').addEventListener('click', function () {
 document.getElementById('scrollRight').addEventListener('click', function () {
     document.querySelector('.imagenes').scrollBy(300, 0);
 });
-document.getElementById('button-submit').addEventListener('click', function () {
+document.getElementById('button-submit').addEventListener('click', function (e) {
     e.preventDefault();
-    alert("Mensaje Enviado");
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+
+    if (name === "" || email === "" || phone === "") {
+        alert("Por favor llena los campos correctamente")
+    } else (alert("Mensaje Enviado"))
+
 });
