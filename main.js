33cbf59b2ce2b6d21a09e142c829d0d9f0381f7b
erickseis <erickseis@gmail.com>
2024-01-10 10:38:30 -0300

    carga

--- main.js
+++ main.js
@@ -6,3 +6,6 @@ document.getElementById('scrollLeft').addEventListener('click', function () {
 document.getElementById('scrollRight').addEventListener('click', function () {
     document.querySelector('.imagenes').scrollBy(300, 0);
 });
+document.getElementById('button-submit').addEventListener('click', function () {
+    alert("Mensaje Enviado");
+});
