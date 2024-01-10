84496eb2458a4ec7e6c25f2ec81887420ddc6b73
erick <erickseis@gmail.com>
2024-01-10 13:50:57 -0300

    carga

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.getElementById('scrollLeft').addEventListener('click', function () {
 document.getElementById('scrollRight').addEventListener('click', function () {
     document.querySelector('.imagenes').scrollBy(300, 0);
 });
-document.getElementById('button-submit').addEventListener('click', function () {
+document.getElementById('myForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
     let name = document.getElementById("name").value;
