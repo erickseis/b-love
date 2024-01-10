acb6cea02244fd54d50fbba92c34d7072155e407
erickseis <erickseis@gmail.com>
2024-01-10 14:11:19 -0300

    carga

--- main.js
+++ main.js
@@ -7,13 +7,10 @@ document.getElementById('scrollRight').addEventListener('click', function () {
     document.querySelector('.imagenes').scrollBy(300, 0);
 });
 document.getElementById('button-submit').addEventListener('click', function () {
-    // e.preventDefault();
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let phone = document.getElementById("phone").value;
-
     if (name === "" || email === "" || phone === "") {
-        alert("Por favor llena los campos correctamente")
+        alert("Por favor llena los campos correctamente");
     }
-
 });
